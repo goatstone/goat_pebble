3b4fb1d320b76ff746a5f78babe66201902310fa
joseherminiocollas <joseherminiocollas@goatstone.com>
2015-06-28 15:45:22 +0000

    Add scrolling text.

--- setDisplay.js
+++ setDisplay.js
@@ -3,12 +3,23 @@
 //var querystring = require('querystring');
 //var http = require('http');
 //var request = require('request');
-
+var msg = 'Welcome to the Pebble Accesory, PebbleFlash!';
+var count = 0;
 function setDisplay(my){
-	//console.log('update local display...');
+	console.log('update local display...', count);
+	
 	my.screen.setCursor(0, 4);
 	var d = new Date();
 	var dS = (d.getHours()) + ':' + (d.getMinutes()) + ':' + d.getSeconds();
-	my.screen.write( dS );            
+	my.screen.write( dS );
+
+	// write bottom half of screen
+	my.screen.setCursor(1,0);
+	my.screen.write('                ');
+	my.screen.setCursor(1,0);
+	//my.screen.write('XXX');
+	var m = msg.substr(count, 20)
+	my.screen.write(m);
+	count = (count>30)? 0 : count + 2 ;       	     
 }
 exports.setDisplay = setDisplay;
